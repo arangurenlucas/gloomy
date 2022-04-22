b865d504d980085250a1acd3e5d0f8b105fdd538
Lucas Aranguren <arangurenlucas@live.com>
2022-04-21 21:37:48 -0300

    isLogged is used from context provider

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import MyContext from '../MyContext';
 
 function MainNavigation() {
-  const isLogged: boolean = true;
+  const { isLogged } = useContext(MyContext);
   return (
     <Routes>
       {isLogged ? (
