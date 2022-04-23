3b32b2b9ca21fda48fcf6bda59472ea07ac88fcb
gmfacundo <gmf.facundo@gmail.com>
2022-04-23 19:59:27 -0300

    console log quitado y se activó la lógica de creación de nuevo evento

--- src/components/Event/NewEvent.tsx
+++ src/components/Event/NewEvent.tsx
@@ -5,14 +5,13 @@ import CreateEvent from './CreateEvent';
 
 function NewEvent() {
   const [newEventClicked, setNewEventClickedState] = useState<boolean>();
-  console.log(newEventClicked);
   
   return (
     <div>
     <button className="newEventButton" onClick={() => setNewEventClickedState(!newEventClicked) }>
       <AiOutlinePlus className="newEventIcon" />
     </button>
-    {/* {newEventClicked ? <CreateEvent /> : null } */}
+    {newEventClicked ? <CreateEvent /> : null }
     </div>
   );
 }
