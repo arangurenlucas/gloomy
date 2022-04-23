4c743b946fb410ed577275287034e3cdd0424839
Lucas Aranguren <arangurenlucas@live.com>
2022-04-23 00:32:29 -0300

    getEvents is requested on useEffect if user is logged

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
-import { useState } from 'react';
+import type { Event } from '../src/interfaces/Event';
+import { useEffect, useState } from 'react';
 import './App.css';
 import MainNavigation from '../src/navigation/MainNavigation';
 import MyContext from '../src/MyContext';
 import Sidebar from './components/Sidebar/Sidebar';
+import { getEvents } from './Service/ServiceAPI';
 
 function App() {
   const [isLogged, setIsLogged] = useState<boolean>(true);
+  const [events, setEvents] = useState<Event[]>([]);
+
+  const getEventList = async () => {
+    try {
+      const data = await getEvents();
+      setEvents(
+        data.docs.map((doc) => {
+          const { eventName, description, eventHost, imageUrl, eventDate, expired } = doc.data();
+          return {
+            eventName,
+            description,
+            eventHost,
+            imageUrl,
+            eventDate,
+            expired,
+            id: doc.id
+          };
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (isLogged) {
+      getEventList();
+    }
+  }, []);
 
   return (
     <MyContext.Provider
       value={{
-        isLogged
+        isLogged,
+        events
       }}
     >
       <div className="App">
