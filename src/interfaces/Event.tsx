fedddc8bc930f1ae1c4a3ff7be497b53df87e135
Lucas Aranguren <arangurenlucasl@gmail.com>
2022-04-27 13:44:39 -0300

    unnecessary new lines  deleted at the end of the file

--- src/interfaces/Event.tsx
+++ src/interfaces/Event.tsx
@@ -4,14 +4,9 @@ export interface Event {
   eventName: string;
   description: string;
   eventCategory: string;
-  eventHost: string; 
-  imageUrl: string; 
-  eventDate: Timestamp; 
+  eventHost: string;
+  imageUrl: string;
+  eventDate: Timestamp;
   expired: boolean;
-  id?: string; 
+  id?: string;
 }
-
-
-
-
-
