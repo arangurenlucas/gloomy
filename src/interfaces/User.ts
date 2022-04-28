071ea4dd3a96b3801d77cf9114b47125a225cf3d
Lucas Aranguren <arangurenlucasl@gmail.com>
2022-04-28 02:01:02 -0300

    interfaces for User and newUser added

---
+++ src/interfaces/User.ts
@@ -0,0 +1,12 @@
+export interface User {
+  uid: string;
+  email: string;
+  displayName: string;
+  photoURL: string;
+}
+
+export interface newUser {
+  email: string;
+  displayName: string;
+  photoURL: string;
+}
