4731c4bef7dc9286cf691c6a8ebb5a5bea6b83fd
Sebastian <sebastian.romero2885@gmail.com>
2022-04-29 00:32:05 -0300

    Se agrego la navegacion a la pagina create-account

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
+import CreateAccount from '../pages/CreateAccount';
 import AuthRoute from '../Service/AuthRoute';
 import Account from '../pages/Account';
 import Events from '../pages/Events';
@@ -12,6 +13,7 @@ function MainNavigation() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/create-account" element={<CreateAccount />} />
       <Route element={<AuthRoute />}>
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
